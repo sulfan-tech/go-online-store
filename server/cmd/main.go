19f1582efa33c8875c1315d03987683f5d71f440
sulfan@fithub.id <sulfan@fithub.id>
2024-06-18 18:00:22 +0700

    Add Docker and Docker Compose configuration

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,64 +1,40 @@
-package mysql
+package main
 
 import (
-	"fmt"
+	"go-online-store/pkg/logger"
+	"go-online-store/server/router"
 	"log"
-	"net/url"
+	"net/http"
 	"os"
 
+	valiator "go-online-store/internal/middleware/validator"
+
+	"github.com/go-playground/validator/v10"
 	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
-var DB *gorm.DB
-
-type DatabaseConfig struct {
-	Username string
-	Password string
-	Host     string
-	Port     string
-	DBName   string
-}
-
-// LoadDatabaseConfig loads the database configuration from environment variables
-func LoadDatabaseConfig() *DatabaseConfig {
-	err := godotenv.Load(".env")
+func main() {
+	// env := "/root/.env"
+	env := ".env"
+	err := godotenv.Load(env)
 	if err != nil {
-		log.Println("Error loading .env file on load db, falling back to environment variables")
+		log.Fatal("Error loading .env file on main :" + err.Error())
 	}
+	log := logger.NewLogger(os.Stdout, "Main")
 
-	dbURL := os.Getenv("CLEARDB_DATABASE_URL")
-	parsedURL, err := url.Parse(dbURL)
-	if err != nil {
-		log.Fatal("Error parsing CLEARDB_DATABASE_URL: ", err)
-	}
+	e := echo.New()
 
-	dbUser := parsedURL.User.Username()
-	dbPass, _ := parsedURL.User.Password()
-	dbHost := parsedURL.Hostname()
-	dbPort := parsedURL.Port()
-	dbName := parsedURL.Path[1:]
+	// Register validator
+	e.Validator = &valiator.CustomValidator{Validator: validator.New()}
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost},
+	}))
 
-	return &DatabaseConfig{
-		Username: dbUser,
-		Password: dbPass,
-		Host:     dbHost,
-		Port:     dbPort,
-		DBName:   dbName,
-	}
-}
-
-// ConnectDatabase initializes the database connection using GORM
-func ConnectDatabase() (*gorm.DB, error) {
-	cfg := LoadDatabaseConfig()
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=True",
-		cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.DBName)
-
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		return nil, err
-	}
-
-	return db, nil
+	e = router.RegisterRouter(e, log)
+	e.Logger.Print("Server is running on port: " + os.Getenv("SERVER_PORT"))
+	log.Info("Server is running on port: " + os.Getenv("SERVER_PORT"))
+	e.Logger.Fatal(e.Start(":" + os.Getenv("SERVER_PORT")))
 }
